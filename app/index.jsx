2fa29c4cf903cd2dd276d0e5275278c1cb62136a
lugatiman <michellegracelugatiman687@gmail.com>
2025-08-22 16:46:45 +0800

    index is changed

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        M Y  G O A L S
+        M Y  S W E E T I E 
       </Text>
       <Link style={styles.link} href="/goals">
         View Your Goals
